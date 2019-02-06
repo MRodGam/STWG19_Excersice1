9c7dc3488829cc5963c63bc037a956e77f870665
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2019-02-06 11:20:39 +0100

    Unittest af Divide overloadmetoden

--- UnittestingCalculator/TestCalculator.Test.Unit/Calculator_DivideOverload_TestUnit.cs
+++ UnittestingCalculator/TestCalculator.Test.Unit/Calculator_DivideOverload_TestUnit.cs
@@ -18,77 +18,77 @@ namespace TestCalculator.Test.Unit
         {
             uut = new Calculator();
         }
-        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at gange to positive tal, af forskellige typer
+        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at dividere to positive tal, af forskellige typer
         [TestCase(2, 2, 1)]
         [TestCase(5, 2.5, 2)]
-        [TestCase(3.6, 7.2, 25.920)]
-        public void Multiply_TwoPositiveNumbers_OutputIsCorrect(double a, double b, double expectedResult)
+        [TestCase(3.6, 7.2, 0.5)]
+        public void Divide_TwoPositiveNumbers_OutputIsCorrect(double a, double b, double expectedResult)
         {
             uut.Accumulator = a;
-            uut.Multiply(b);
+            uut.Divide(b);
             Assert.That(uut.Accumulator, Is.EqualTo(expectedResult).Within(0.01)); // Denne within angiver graden af decimaltal man betragter. I dette tilfælde er det inden for to decimaler.
         }
 
-        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at gange to negtive tal, af forskellige typer
-        [TestCase(-2, -2, 4)]
-        [TestCase(-5, -5, 25)]
-        [TestCase(-3.6, -7.2, 25.920)]
-        public void Multiply_TwoNegativeNumbers_OutputIsCorrect(double a, double b, double expectedResult)
+        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at dividere to negtive tal, af forskellige typer
+        [TestCase(-2, -2, 1)]
+        [TestCase(-5, -2.5, 2)]
+        [TestCase(-3.6, -7.2, 0.5)]
+        public void Divide_TwoNegativeNumbers_OutputIsCorrect(double a, double b, double expectedResult)
         {
             uut.Accumulator = a;
-            uut.Multiply(b);
+            uut.Divide(b);
             Assert.That(uut.Accumulator, Is.EqualTo(expectedResult).Within(0.01));
         }
 
 
-        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at gange et positivt tal og et negativt
-        [TestCase(2, -2, -4)]
-        [TestCase(5, -5, -25)]
-        [TestCase(3.6, -7.2, -25.920)]
-        public void Multiply_OnePositiveOneNegativeNumber_OutputIsCorrect(double a, double b, double expectedResult)
+        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at dividere et positivt tal og et negativt
+        [TestCase(2, -2, -1)]
+        [TestCase(5, -2.5, -2)]
+        [TestCase(3.6, -7.2, -0.5)]
+        public void Divide_OnePositiveOneNegativeNumber_OutputIsCorrect(double a, double b, double expectedResult)
         {
             uut.Accumulator = a;
-            uut.Multiply(b);
+            uut.Divide(b);
             Assert.That(uut.Accumulator, Is.EqualTo(expectedResult).Within(0.01));
         }
 
-        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at gange et negativt og et positivt
-        [TestCase(-2, 2, -4)]
-        [TestCase(-5, 5, -25)]
-        [TestCase(-3.6, 7.2, -25.920)]
-        public void Multiply_OneNegativeOnePositiveNumber_OutputIsCorrect(double a, double b, double expectedResult)
+        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at dividere et negativt og et positivt
+        [TestCase(-2, 2, -1)]
+        [TestCase(-5, 2.5, -2)]
+        [TestCase(-3.6, 7.2, -0.5)]
+        public void Divide_OneNegativeOnePositiveNumber_OutputIsCorrect(double a, double b, double expectedResult)
         {
             uut.Accumulator = a;
-            uut.Multiply(b);
+            uut.Divide(b);
             Assert.That(uut.Accumulator, Is.EqualTo(expectedResult).Within(0.01));
         }
 
-        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at gange et positivt tal med 0
-        [TestCase(1, 0, 0)]
-        [TestCase(1.5, 0, 0)]
-        public void Multiply_OnePositiveNumberZero_OutputIsCorrect(double a, double b, double expectedResult)
+        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at dividere et positivt tal med 0
+        [TestCase(1, 0, 1)]
+        [TestCase(1.5, 0, 1.5)]
+        public void Divide_OnePositiveNumberZero_OutputIsCorrect(double a, double b, double expectedResult)
         {
             uut.Accumulator = a;
-            uut.Multiply(b);
+            uut.Divide(b);
             Assert.That(uut.Accumulator, Is.EqualTo(expectedResult).Within(0.01));
         }
 
-        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at gange et negativt tal med 0
-        [TestCase(-1, 0, 0)]
-        [TestCase(-1.5, 0, 0)]
-        public void Multiply_OneNegativeNumberZero_OutputIsCorrect(double a, double b, double expectedResult)
+        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at dividere et negativt tal med 0
+        [TestCase(-1, 0, -1)]
+        [TestCase(-1.5, 0, -1.5)]
+        public void Divide_OneNegativeNumberZero_OutputIsCorrect(double a, double b, double expectedResult)
         {
             uut.Accumulator = a;
-            uut.Multiply(b);
+            uut.Divide(b);
             Assert.That(uut.Accumulator, Is.EqualTo(expectedResult).Within(0.01));
         }
 
-        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at gange 0 med 0
+        // Jeg tester med brug af testcases, hvorvidt metoden er er i stand til at dividere 0 med 0
         [TestCase(0, 0, 0)]
-        public void Multiply_ZeroToZero_OutputIsCorrect(double a, double b, double expectedResult)
+        public void Divide_ZeroToZero_OutputIsCorrect(double a, double b, double expectedResult)
         {
             uut.Accumulator = a;
-            uut.Multiply(b);
+            uut.Divide(b);
             Assert.That(uut.Accumulator, Is.EqualTo(expectedResult).Within(0.01));
         }
     }
